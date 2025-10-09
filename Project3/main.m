12ce435d7614bb61ac06b4dc383089ba1e7874ee
Dylan D <djd1494@gmail.com>
2025-10-09 16:55:01 -0400

    Validate transition matrices

--- Project3/main.m
+++ Project3/main.m
@@ -13,34 +13,36 @@ p3 = 0.2; p4 = -0.5;
 p5 = 10; p6 = pi/2;
 p = [p1; p2; p3; p4; p5; p6];
 
-% Integrating Original ODE
-nlfunc = @(t, x) xdot(t, x, p);
-[t, sol] = ode45(nlfunc, t, state0);
-
+% Initial Conditions
 phi0 = eye(2);
 psi0 = zeros(2, 6);
+state0 = [state0;phi0(:); psi0(:)];
+
+% Integrating ODE for system and for transition matrices
+nlfunc = @(t, state) dynamics_LP(t, state, p);
+[t, sol] = ode45(nlfunc, t, state0);
+xN = sol(:, 1:2)
 
 % Adding measurement error
-
-
-state0 = [state0;phi0(:); psi0(:)]
-f = dynamics_LP(0, state0, p)
+% sigma = 0.1;
+% rng(2025) % Setting seed for reusability
+% xtilde = xN + rand(size(xN))*sigma;
 
 %%
 subplot(1, 3, 1)
-plot(t, sol(:, 1))
+plot(t, xN(:, 1))
 title('$x(t)$ vs $t$', 'Interpreter', 'latex')
 xlabel('$t$', 'Interpreter', 'latex')
 ylabel("$x(t)$", 'Interpreter', 'latex')
 
 subplot(1, 3, 2)
-plot(t, sol(:, 2))
+plot(t, xN(:, 2))
 title('$\dot{x}(t)$ vs $t$', 'Interpreter', 'latex')
 xlabel('$t$', 'Interpreter', 'latex')
 ylabel("$\dot{x}(t)$", 'Interpreter', 'latex')
 
 subplot(1, 3, 3)
-plot(sol(:, 1), sol(:, 2))
+plot(xN(:, 1), xN(:, 2))
 title('$\dot{x}(t)$ vs $x(t)$', 'Interpreter', 'latex')
 xlabel('$x(t)$', 'Interpreter', 'latex')
 ylabel("$\dot{x}(t)$", 'Interpreter', 'latex')
