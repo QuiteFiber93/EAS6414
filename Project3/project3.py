27ab18c828bd5a2bb46ee5408a455b8ceeaf3604
Dylan D <djd1494@gmail.com>
2025-10-16 20:50:25 -0400

    Validate matrices

--- Project3/project3.py
+++ Project3/project3.py
@@ -17,6 +17,11 @@ p       = np.array([0.05, 4, 0.2, -0.5, 10, pi/2])
 tspan   = [0, 300]
 teval   = [i/10 for i in range(1, 3001)]
 
+# Settings to change
+sigma = 0.1 # Given Standard Deviation 
+maxiter = 20 # Max iteration count for GLSDC 
+scale_factor = 0.9 # Scale factor for GLSDC Guess
+
 '''
 System Dynamics
 '''
@@ -37,20 +42,20 @@ def dynamics(t: float, y: NDArray[np.number], p: NDArray[np.number]) -> NDArray:
     
     # Partial derivative of f wrt x and xdot
     A = np.array([
-                [0, 1],
-                [-(p2 + 3*p3*x[0]**2), -p1]
+                    [0, 1],
+                    [-(p2 + 3*p3*x[0]**2), -p1]
                 ])
     
     # Partial derivative of f wrt p
     dfdp = np.array([
-                    [0, 0, 0, 0, 0, 0],
-                    [-x[1], -x[0], -x[0]**3, -sin(p5*t + p6), -p4*t*cos(p5*t+p6), -p4*cos(p5*t+p6)]
+                        [0, 0, 0, 0, 0, 0],
+                        [-x[1], -x[0], -x[0]**3, -sin(p5*t + p6), -p4*t*cos(p5*t+p6), -p4*cos(p5*t+p6)]
                     ])
     
     # Equation for xdot
     xdot = np.array([
-                    x[1],
-                    -( p1*x[1] + p2*x[0] + p3*x[0]**3 + p4*sin(p5*t + p6) )
+                        x[1],
+                        -( p1*x[1] + p2*x[0] + p3*x[0]**3 + p4*sin(p5*t + p6) )
                     ])
     
     # Definitions of PhiDot and PsiDot
@@ -88,12 +93,11 @@ Adding Measurement Noise
 print('-'*50)
 print('Adding Gaussian Noise to x(t)')
 # Given Standard Deviation
-sigma = 0.1
 print(f'\t- mean  = {0}')
 print(f'\t- sigma = {sigma}')
 
 # Setting numpy seed
-np.random.default_rng(2025)
+rng = np.random.default_rng(2025)
 
 # adding gaussian noise to measurements
 ytilde = measured_states.y[0, :] + np.random.normal(loc=0, scale=sigma, size = len(teval))
@@ -123,7 +127,6 @@ xdotvt.set_ylabel(r'$\dot{x}(t)$ vs $t$')
 
 solution_fig.tight_layout()
 
-
 # Plotting phase portrait
 phase_portrait = plt.figure()
 plase_portrait_ax = phase_portrait.gca()
@@ -133,7 +136,6 @@ plase_portrait_ax.plot(simulated_motion.y[0, :], simulated_motion.y[1, :])
 plase_portrait_ax.set_title('Phase Portrait of System')
 plase_portrait_ax.set_xlabel(r'$x(t)$ vs $t$')
 plase_portrait_ax.set_ylabel(r'$\dot{x}(t)$ vs $t$')
-
 
 # Plotting measurements against x(t)
 measurement_fig = plt.figure(figsize = (10, 3))
@@ -156,22 +158,14 @@ Implementing GLSDC from Tapley, Shultz, and Born 2004
 z_true = np.concatenate([x0, p])
 
 # Adjusting to create an initial guess
-scale_factor = 0.85
 z = scale_factor * z_true
-
-# Covariance Matrix = sigma
-R = sigma**2 * np.eye(1)
-W = np.linalg.inv(R)
 
 # Initializing cost values used for determining convergence
 old_cost = np.inf
-new_cost = 0
-
-# Max Iterations for GLSDC Loop
-maxiter = 10
+new_cost = 1
 
 # Error Tolerance for GLSDC
-tol = 1E-4
+tol = 1E-5
 
 # Entering GLSDC Loop
 for i in range(maxiter):
@@ -186,24 +180,28 @@ for i in range(maxiter):
     print(f'x0 = {np.array2string(x0_guess)}')
     print(f'p  = {np.array2string(p_guess)}')
     
+    
     old_cost = new_cost
     new_cost = 0
+    
+    # Values for solving the normal equations Lambda @ deltaZ = N
     Lambda = np.zeros((8, 8))
     N = np.zeros((8, 1))
+    
     initial_state_guess = np.concatenate([x0_guess, phi0.flatten(), psi0.flatten()])
     
     # Integrate trajectory based of of x0_guess and p_guess
     print('-'*50)
     print('Beginning Integration')
     glsdc_guess_traj = solve_ivp(
-                        dynamics,
-                        tspan,
-                        initial_state_guess,
-                        t_eval=teval,
-                        method = 'DOP853',
-                        rtol = 1E-9,
-                        atol = 1E-9,
-                        args = (p_guess,)
+                            dynamics,
+                            tspan,
+                            initial_state_guess,
+                            t_eval=teval,
+                            method = 'DOP853',
+                            rtol = 1E-8,
+                            atol = 1E-8,
+                            args = (p_guess,)
                         )
     
     # Parsing through data from integration
@@ -215,8 +213,9 @@ for i in range(maxiter):
     
     for k, t_k in enumerate(teval):
         err[k, 0] = ytilde[k] - glsdc_guess_traj.y[0, k]
-        # TODO: Implement R which changes over time
-        R = np.diag([t_k*sigma**2])
+        
+        # Creating Weight Matrix
+        R = np.diag([(1 + t_k)*sigma**2])
         W = np.linalg.inv(R)
         
         # Pulling Variational Matrices
@@ -224,50 +223,42 @@ for i in range(maxiter):
         psi = glsdc_guess_traj.y[6:, k].reshape(2, 6)
         H_i = np.concatenate([ np.squeeze(np.array([[1, 0]]) @ phi), np.squeeze(np.array([[1, 0]]) @ psi)]).reshape(1, 8)
         
+        # Adding to matrices for normal equation
         Lambda += H_i.T @ W @ H_i
         N += H_i.T @ W @ err[k].reshape(1, 1)
         
+        # Adding error to weighted least squares cost
         new_cost += err[k].T @ W @ err[k]
 
+    delta_z = np.linalg.solve(Lambda, N).reshape(8)
+    
     print('-'*50)
     print(f'Least Squares Error Cost: {new_cost}')
-    
-    delta_z = np.linalg.solve(Lambda, N).reshape(8)
+    print(f'Change in Error Cost:\t {new_cost - old_cost}')
+    print(f'Relative Change of new_cost: {(new_cost - old_cost) / old_cost}')
     print(f'Delta z: \n{np.array2string(delta_z)}')
     
     # Checking if error tolerance is met
-    if abs(new_cost - old_cost) / new_cost <= tol:
+    if abs(new_cost - old_cost) / old_cost <= tol:
         break
     
     z += delta_z
     
     # Keep between zero and 2*pi
     z[-1] = z[-1] % (2*pi)
-    
-    
 
 print('='*50)
 print('Exiting GLSDC Loop')
-if abs(new_cost - old_cost) / new_cost > tol:
+if abs(new_cost - old_cost) / old_cost > tol:
     print('='*50)
     print(f'GLSDC Failed to converge to a solution which met tol = {tol} within {maxiter} iterations')
 
-print('Final Value')
+print('Final Estimate')
 x0_guess = z[:2]
 p_guess=  z[2:]
 
 print(f'x0 = {np.array2string(x0_guess)}')
 print(f'p  = {np.array2string(p_guess)}')   
-  
-# solution_comp_plot = plt.figure(figsize = (10, 3))
-# solution_comp_ax = solution_comp_plot.gca()
-# solution_comp_ax.plot(measured_states.t, measured_states.y[0, :], '--', label = 'True Solution')
-# solution_comp_ax.plot(glsdc_guess_traj.t, glsdc_guess_traj.y[0, :], label = 'GLSDC Solution')
-# solution_comp_ax.set_title('Comparison of True Solution to GLSDC Solution')
-# solution_comp_ax.set_xlabel(r'$t$')
-# solution_comp_ax.set_xlim(tspan)
-# solution_comp_ax.set_ylabel(r'$x(t)$')
-# solution_comp_ax.legend()
     
 doPlot = False
 if doPlot:
