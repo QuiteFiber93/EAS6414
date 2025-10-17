c0e07019f5c041d13923b684648bcb6fd5baecf4
Dylan D <djd1494@gmail.com>
2025-10-17 19:04:21 -0400

    Convert GLSDC to function, finish task 3 and 4

--- Project3/project3.py
+++ Project3/project3.py
@@ -8,25 +8,44 @@ EAS 6414 Project 3
 Dylan D'Silva
 '''
 
+# Settings to change how file runes
+sigma           = 0.1 # Given Standard Deviation 
+maxiter         = 30 # Max iteration count for GLSDC 
+scale_factor    = 0.98 # Scale factor for GLSDC Guess
+doPlot          = False # Used to control whether the generated plots are shown
+tol             = 1E-3 # Error Tolerance for GLSDC
+
+
+# Delimeter strings for print statements
+delimeter_equals = '='*80
+delimeter_dash = '-'*80
 
 '''
+********************************************************************
 Given Values
+********************************************************************
 '''
-x0      = np.array([2, 0])
-p       = np.array([0.05, 4, 0.2, -0.5, 10, pi/2])
-tspan   = [0, 300]
-teval   = [i/10 for i in range(1, 3001)]
+x0              = np.array([2, 0]) # Initial Conditions
+p               = np.array([0.05, 4, 0.2, -0.5, 10, pi/2]) # True Parameters
+tspan           = [0, 300] # Initial and Final Times of Simulation
+teval           = [i/10 for i in range(1, 3001)] # Times at which system is sampled for measurement
 
-# Settings to change
-sigma = 0.1 # Given Standard Deviation 
-maxiter = 20 # Max iteration count for GLSDC 
-scale_factor = 0.9 # Scale factor for GLSDC Guess
+print(delimeter_equals + '\nEAS 6414 Project 3: Initial State and Parameter Estimation\n' + delimeter_equals)
+
+print('\nGiven Values\n' + delimeter_dash)
+print(f'x0                              = {np.array2string(x0)}')
+print(f'p                               = {np.array2string(p)}')
+print(f'Measurement Covariance          = {sigma}^2')
+print(f'Maximum Iteration for GLSDC     = {maxiter}')
+print(f'Scale factor for initial guess  = {scale_factor}\n')
 
 '''
+********************************************************************
 System Dynamics
+********************************************************************
 '''
 def dynamics(t: float, y: NDArray[np.number], p: NDArray[np.number]) -> NDArray:
-    """Integrates the system dynamics and variational matrices
+    """Integrates the system dynamics and variational matrices.
 
     Args:
         t (float): time variable
@@ -66,7 +85,9 @@ def dynamics(t: float, y: NDArray[np.number], p: NDArray[np.number]) -> NDArray:
     return np.concatenate([xdot, phidot.flatten(), psidot.flatten()])
 
 '''
+********************************************************************
 Integrating System Dynamics
+********************************************************************
 '''
 
 # Initial State
@@ -74,11 +95,8 @@ phi0    = np.eye(2)
 psi0    = np.zeros((2, 6))
 state0  = np.concatenate([x0, phi0.flatten(), psi0.flatten()])
 
-print('='*50)
-print(f'x0 = {np.array2string(x0)}')
-print(f'p  = {np.array2string(p)}')
-print('-'*50)
-print("Beginning Integration of System Dynamics ...")
+print(delimeter_equals + '\nTask 1: Simulating Motion, Measurements, and Validating Matrices\n' + delimeter_equals)
+print("\nBeginning Integration of System Dynamics ...")
 
 # Integrating System
 simulated_motion    = solve_ivp(dynamics, tspan, state0, args = (p,), rtol = 1E-10, atol=1E-10)
@@ -87,24 +105,24 @@ measured_states     = solve_ivp(dynamics, tspan, state0, t_eval = teval, args = 
 print('Integration Complete')
 
 """
+********************************************************************
 Adding Measurement Noise
+********************************************************************
 """
 
-print('-'*50)
-print('Adding Gaussian Noise to x(t)')
-# Given Standard Deviation
-print(f'\t- mean  = {0}')
-print(f'\t- sigma = {sigma}')
-
 # Setting numpy seed
-rng = np.random.default_rng(2025)
+rng = np.random.default_rng(seed=2025)
 
 # adding gaussian noise to measurements
 ytilde = measured_states.y[0, :] + np.random.normal(loc=0, scale=sigma, size = len(teval))
 
 '''
+********************************************************************
 Plotting System
+********************************************************************
 '''
+
+print('Plotting Solution ...')
 # Plotting Solution State Variables vs Time
 solution_fig, axs = plt.subplots(2, 1, figsize = (10, 4), sharex='col')
 xvt, xdotvt = axs
@@ -127,6 +145,7 @@ xdotvt.set_ylabel(r'$\dot{x}(t)$ vs $t$')
 
 solution_fig.tight_layout()
 
+print('Generating Phase Portrait ...')
 # Plotting phase portrait
 phase_portrait = plt.figure()
 plase_portrait_ax = phase_portrait.gca()
@@ -137,21 +156,31 @@ plase_portrait_ax.set_title('Phase Portrait of System')
 plase_portrait_ax.set_xlabel(r'$x(t)$ vs $t$')
 plase_portrait_ax.set_ylabel(r'$\dot{x}(t)$ vs $t$')
 
+print('Overlaying Measurements ...')
 # Plotting measurements against x(t)
 measurement_fig = plt.figure(figsize = (10, 3))
 measurement_ax = measurement_fig.gca()
 
 measurement_ax.plot(simulated_motion.t, simulated_motion.y[0, :], linewidth=1, label = 'True Motion')
 measurement_ax.scatter(measured_states.t, ytilde, s = 1, c= 'orange', label = 'Measured Position')
+
 measurement_ax.set_title("Solution Position vs Time")
 measurement_ax.set_xlim(tspan)
 measurement_ax.set_xlabel(r"$t$")
 measurement_ax.set_ylabel(r'$x(t)$ vs $t$')
 measurement_ax.legend()
 
+if doPlot:
+    solution_fig.savefig('Images/solution_fig.png', format='png', dpi=1440)
+    phase_portrait.savefig('Images/phase_portrait.png', format='png', dpi=1440)
+    measurement_fig.savefig('Images/measurements_fig.png', format='png', dpi=1440)
+    print('Plots Generated and Saved\n')
+
 
 '''
+********************************************************************
 Implementing GLSDC from Tapley, Shultz, and Born 2004
+********************************************************************
 '''
 
 # Estimating z = [x^T, p^T]^T
@@ -164,35 +193,29 @@ z = scale_factor * z_true
 old_cost = np.inf
 new_cost = 1
 
-# Error Tolerance for GLSDC
-tol = 1E-5
 
+print(delimeter_equals + '\nTask 2: GLSDC Algorithm\n'+delimeter_equals)
 # Entering GLSDC Loop
+print('\nIteration   x(0)   xdot(0)   p1      p2      p3     p4     p5     p6     Cost\n' + delimeter_dash)
 for i in range(maxiter):
-    print("="*50)
-    print('Beginning GLSDC Iteration ', i+1)
     
     # Initialize for iteration
-    print('-'*50)
-    print('Starting guess for parameters')
     x0_guess = z[:2]
     p_guess = z[2:]
-    print(f'x0 = {np.array2string(x0_guess)}')
-    print(f'p  = {np.array2string(p_guess)}')
     
     
     old_cost = new_cost
+    print('')
     new_cost = 0
     
     # Values for solving the normal equations Lambda @ deltaZ = N
     Lambda = np.zeros((8, 8))
-    N = np.zeros((8, 1))
+    N = np.zeros((8, 1)) 
     
     initial_state_guess = np.concatenate([x0_guess, phi0.flatten(), psi0.flatten()])
     
     # Integrate trajectory based of of x0_guess and p_guess
-    print('-'*50)
-    print('Beginning Integration')
+
     glsdc_guess_traj = solve_ivp(
                             dynamics,
                             tspan,
@@ -205,7 +228,6 @@ for i in range(maxiter):
                         )
     
     # Parsing through data from integration
-    print('Compelted Integration ...')
     if glsdc_guess_traj.status == -1:
         print(glsdc_guess_traj.message)
         
@@ -215,7 +237,7 @@ for i in range(maxiter):
         err[k, 0] = ytilde[k] - glsdc_guess_traj.y[0, k]
         
         # Creating Weight Matrix
-        R = np.diag([(1 + t_k)*sigma**2])
+        R = np.diag([(1+t_k)*sigma**2]) # (1+t_k)*sigma**2
         W = np.linalg.inv(R)
         
         # Pulling Variational Matrices
@@ -230,36 +252,75 @@ for i in range(maxiter):
         # Adding error to weighted least squares cost
         new_cost += err[k].T @ W @ err[k]
 
-    delta_z = np.linalg.solve(Lambda, N).reshape(8)
-    
-    print('-'*50)
-    print(f'Least Squares Error Cost: {new_cost}')
-    print(f'Change in Error Cost:\t {new_cost - old_cost}')
-    print(f'Relative Change of new_cost: {(new_cost - old_cost) / old_cost}')
-    print(f'Delta z: \n{np.array2string(delta_z)}')
-    
     # Checking if error tolerance is met
     if abs(new_cost - old_cost) / old_cost <= tol:
         break
     
+    # If not converged, calculated next step in z
+    delta_z = np.linalg.solve(Lambda, N).reshape(8)
+    
+    print(f'{i+1}   {x0_guess[0]:>10.3f}   {x0_guess[1]:0.3f}   {p_guess[0]:0.3f}' 
+          + f'  {p_guess[1]:0.3f}   {p_guess[2]:0.3f}   {p_guess[3]:0.3f}   {p_guess[4]:0.3f}   {p_guess[5]:0.3}   {new_cost:0.3}')
+    
+    # Stepping z
     z += delta_z
     
     # Keep between zero and 2*pi
     z[-1] = z[-1] % (2*pi)
 
-print('='*50)
-print('Exiting GLSDC Loop')
+print(delimeter_dash)
 if abs(new_cost - old_cost) / old_cost > tol:
-    print('='*50)
-    print(f'GLSDC Failed to converge to a solution which met tol = {tol} within {maxiter} iterations')
+    print(delimeter_equals + f'\nGLSDC Failed to converge to a solution which met tol = {tol} within {maxiter} iterations')
 
 print('Final Estimate')
 x0_guess = z[:2]
-p_guess=  z[2:]
+p_guess = z[2:]
 
 print(f'x0 = {np.array2string(x0_guess)}')
 print(f'p  = {np.array2string(p_guess)}')   
     
-doPlot = False
-if doPlot:
-    plt.show()
+'''
+********************************************************************
+Covariance Matrix Calculations
+********************************************************************
+'''
+
+# Times at which system is sampled
+sample_times = [100, 200, 300]
+print('\n' + delimeter_equals + '\nProducing Covariance Ellipses\n' + delimeter_equals)
+print(f'\nCalculating P(t) at t = 0, 100, 200, 300')
+
+# Creating variable to hold covariance matrix at each sample time
+Pt = np.zeros((4, 8, 8))
+
+# Calculating P(t_0) as inv(Lambda)
+P0 = np.linalg.inv(Lambda)
+Pt[0, :, :] = P0
+
+# Using solution calculated from GLSDC to construct Phi(t) and Psi(t)
+for k,t in enumerate(sample_times):
+    PhiPsiVec = glsdc_guess_traj.y[2:, glsdc_guess_traj.t==t]
+    Phi = PhiPsiVec[:4].reshape(2,2)
+    Psi = PhiPsiVec[4:].reshape(2, 6)
+    dzdz0 = np.block([[Phi, Psi], [np.zeros((6, 2)), np.eye(6)]])
+    Pt[k+1, :, :] = dzdz0 @ P0 @ dzdz0.T
+
+
+# Creating cos and sin of parameter for ellipse
+t = np.linspace(0, 2*pi, 100)
+ellipse = np.array([cos(t), sin(t)])
+
+covar_ellipse_plot, covar_ellipse_axs = plt.subplots(2, 2)
+for k in range(4):
+    # Extract Px (2x2 matrix in upper left corner) from P(t)
+    Px = Pt[k, :2, :2]
+    
+    # Get  Eigenvalues and Eigen Vectors of Px
+    D, V = np.linalg.eig(Px)
+    print(D)
+
+'''
+********************************************************************
+Sample Statistics
+********************************************************************
+'''
