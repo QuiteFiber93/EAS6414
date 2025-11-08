195e990ca6aa6d27892edca74b6e908008ae64bf
Dylan D <djd1494@gmail.com>
2025-11-08 00:21:12 -0500

    Finished Project 4

--- Project4/main.m
+++ Project4/main.m
@@ -57,6 +57,7 @@ rho_obsv_true_motion = observer_range(rho_true_motion, obsv_lat, LST_true);
 h_cords_true_motion = horizontal_coordinates(rho_obsv_true_motion);
 
 % Figure Plotting true trajectory
+% Creating subplots for x, y, and z
 fig = tiledlayout(3, 1);
 ax1 = nexttile;
 plot(ax1, t, true_motion(:, 1))
@@ -73,8 +74,9 @@ xlabel('t (s)')
 linkaxes([ax1, ax2, ax3], 'x')
 title(fig, 'Earth Fixed Position');
 
-exportgraphics(gca, 'Images/body_fixed_pos.png','Resolution',300)
+exportgraphics(gcf, 'Images/body_fixed_pos.png','Resolution',300)
 
+% Plotting x, y, and z velocities in subplots and saving the figure
 fig = tiledlayout(3, 1);
 ax1 = nexttile;
 plot(ax1, t, true_motion(:, 4))
@@ -88,10 +90,9 @@ ax3 = nexttile;
 plot(ax3, t, true_motion(:, 6))
 ylabel('zdot(t) (km/s)')
 xlabel('t (s)')
-linkaxes([ax1, ax2, ax3], 'x')
 title(fig, 'Earth Fixed Velocity');
 
-exportgraphics(gca, 'Images/body_fixed_vel.png','Resolution',300)
+exportgraphics(gcf, 'Images/body_fixed_vel.png','Resolution',300)
 
 figure
 % converting true trajectory from spherical (horizontal) frame to cartesian
@@ -114,33 +115,63 @@ zlabel('z (km)')
 % Saving plot
 exportgraphics(gca, 'Images/measurements.png','Resolution',300)
 
-% Plotting trajectory individually vs time
+% Plotting x, y, z trajectory relative to the observer vs time and
+% overlaying measurements
 figure;
 ax1 = subplot(3, 1, 1);
 hold on
-plot(ax1, t, x_obsv)
-scatter(ax1, tmeas, x_meas)
+plot(ax1, t, x_obsv, 'DisplayName','True')
+scatter(ax1, tmeas, x_meas, 10, 'filled', 'DisplayName','Measured')
 hold off
 ylabel('x(t) (km)')
+legend('Location','southeast')
 
 ax2 = subplot(3, 1, 2);
 hold on
-plot(ax2, t, y_obsv)
-scatter(ax2, tmeas, y_meas)
+plot(ax2, t, y_obsv, 'DisplayName','True')
+scatter(ax2, tmeas, y_meas, 10, 'filled', 'DisplayName','Measured')
 hold off
 ylabel('y(t) (km)')
+legend('Location','southeast')
 
 ax3 = subplot(3, 1, 3);
 hold on
-plot(ax3, t, z_obsv)
-scatter(ax3, tmeas, z_meas)
+plot(ax3, t, z_obsv, 'DisplayName','True')
+scatter(ax3, tmeas, z_meas, 10, 'filled', 'DisplayName','Measured')
 hold off
 ylabel('z(t) (km)')
 xlabel('t (s)')
+legend('Location','southeast')
 
-linkaxes([ax1, ax2, ax3], 'x');
 sgtitle('Position of Satellite Relative to Observer')
 exportgraphics(gcf, 'Images/cartesian_pos_measurements.png', 'Resolution',300)
+
+% Creating a figure which shows the altitude and azimuth of the satellite
+% overhead from frame of observer
+figure
+polarplot(az_true, 90 - rad2deg(el_true), 'DisplayName','True Trajectory')
+hold on
+polarscatter(measurements(:, 2), 90 - rad2deg(measurements(:, 3)), 7, 'filled', 'DisplayName','Measurements')
+hold off
+title('Polar Plot for Altitude and Azimuth')
+rlim([0, 90])
+ax = gca;
+ax.RTick = 0:30:90;
+ax.RTickLabel = {'90°', '60°', '30°', '0°'};
+exportgraphics(gcf, 'Images/alt_az_plot.png', 'Resolution',300)
+
+% Plotting Range vs Range Measurements
+figure
+plot(t, rho_true, 'DisplayName','True Range')
+hold on
+scatter(tmeas, measurements(:, 1), 10, 'filled', 'DisplayName','Range Measurements')
+hold off
+title('True Range vs Range Measurements')
+xlabel('t (s)')
+ylabel('Range (km)')
+legend()
+exportgraphics(gcf, 'Images/range_plot.png', 'Resolution',300)
+
 
 %% GLSDC
 r0hat = [6990; 1; 1];
@@ -151,8 +182,6 @@ maxiter = 10;
 tol = 1E-3;
 dynamics = @(t, y) twobody_STM(t, y, mu);
 [glsdc_estimate, Lambda] = glsdc(dynamics, x0hat, measurements, tol, R, tmeas, maxiter, R_obsv, LST, obsv_lat);
-
-
 
 %% Get Covariance of GLSDC Estimate
 % P is the inverse of the covariance matrix
@@ -168,21 +197,135 @@ for n = 1:nruns
     mc_results(n, :) = mc_estimate';
 end
 
-% Calculating mean and covariance of estimates from montecarlo runs
+% Calculating mean and covariance of estimates from monte carlo runs
 monte_carlo_estimate = mean(mc_results)';
 P_mc = cov(mc_results);
+
+%% Plotting the Trajectories according to the MC and GLSDC Estimates
+% Integrating using estimates
+func = @(t, y) twobody(t, y, mu);
+options = odeset('RelTol',1E-8, 'AbsTol',1E-10);
+[t, true_traj] = ode45(func, tspan, y0, options);
+[t, glsdc_traj] = ode45(func, t, glsdc_estimate, options);
+[t, monte_carlo_traj] = ode45(func, t, monte_carlo_estimate, options);
+
+% This figure will create a 3d plot of all 3 trajectories, the save it
+figure
+plot3(true_traj(:, 1), true_traj(:, 2), true_traj(:, 3), 'DisplayName', 'True')
+hold on
+plot3(glsdc_traj(:, 1), glsdc_traj(:, 2), glsdc_traj(:, 3), 'DisplayName', 'GLSDC')
+plot3(monte_carlo_traj(:, 1), monte_carlo_traj(:, 2), monte_carlo_traj(:, 3), 'DisplayName', 'Monte Carlo')
+hold off
+title('Trajectory Comparison')
+xlabel('x (km)')
+ylabel('y (km)')
+zlabel('z (km)')
+legend('Location','northeast')
+grid()
+exportgraphics(gcf, 'Images/trajectory_comparison.png', 'Resolution', 300)
+
+% This figure will create a 3d plot showing the errors in trajectory for
+% the GLSDC and Monte Carlo Estimates, then save it
+figure
+plot3(true_motion(:, 1) - glsdc_traj(:, 1), true_motion(:, 2) - glsdc_traj(:, 2), true_motion(:, 3) - glsdc_traj(:, 3), 'DisplayName', 'GLSDC')
+hold on
+plot3(true_motion(:, 1) - monte_carlo_traj(:, 1), true_motion(:, 2) - monte_carlo_traj(:, 2), true_motion(:, 3) - monte_carlo_traj(:, 3), 'DisplayName', 'Monte Carlo')
+hold off
+title('Estimate Trajectory Error')
+xlabel('$\Delta x$ (km)', 'Interpreter','latex')
+ylabel('$\Delta y$ (km)', 'Interpreter','latex')
+zlabel('$\Delta z$ (km)', 'Interpreter','latex')
+legend('Location','northeast')
+grid()
+exportgraphics(gcf, 'Images/estimate_trajectory_error.png', 'Resolution', 300)
+
+% This figure will create a series of plots which show the estimated
+% trajectory errors for x, y, and z, then save it
+% It uses the tiledlayout, which is like subplot
+fig = tiledlayout(3, 1);
+nexttile;
+hold on
+plot(t, true_motion(:, 1) - glsdc_traj(:, 1), 'DisplayName','GLSDC')
+plot(t, true_motion(:, 1) - monte_carlo_traj(:, 1), 'DisplayName','Monte Carlo')
+hold off
+title('$x(t)$ Error', 'Interpreter','latex')
+ylabel('$\Delta x$ (km)', 'Interpreter','latex')
+legend()
+grid()
+
+nexttile;
+hold on
+plot(t, true_motion(:, 2) - glsdc_traj(:, 2), 'DisplayName','GLSDC')
+plot(t, true_motion(:, 2) - monte_carlo_traj(:, 2), 'DisplayName','Monte Carlo')
+hold off
+title('$y(t)$ Error', 'Interpreter','latex')
+ylabel('$\Delta y$ (km)', 'Interpreter','latex')
+legend()
+grid()
+
+nexttile;
+hold on
+plot(t, true_motion(:, 3) - glsdc_traj(:, 3), 'DisplayName','GLSDC')
+plot(t, true_motion(:, 3) - monte_carlo_traj(:, 3), 'DisplayName','Monte Carlo')
+hold off
+title('$z(t)$ Error', 'Interpreter','latex')
+xlabel('t (s)')
+ylabel('$\Delta z$ (km)', 'Interpreter','latex')
+legend()
+grid()
+sgtitle('Trajectory Estimate Position Error')
+exportgraphics(gcf, 'Images/traj_estimate_pos_err.png', 'Resolution',300)
+
+% This figure will create a series of plots which show the estimated
+% trajectory errors for x, y, and z velocities, then save it
+% It uses the tiledlayout, which is like subplot
+fig = tiledlayout(3, 1);
+nexttile;
+hold on
+title('$\dot{x}(t)$ Error', 'Interpreter','latex')
+plot(t, true_motion(:, 4) - glsdc_traj(:, 4), 'DisplayName','GLSDC')
+plot(t, true_motion(:, 4) - monte_carlo_traj(:, 4), 'DisplayName','Monte Carlo')
+hold off
+ylabel('$\Delta \dot{z}$ (km)', 'Interpreter','latex')
+legend()
+grid()
+
+nexttile;
+hold on
+plot(t, true_motion(:, 5) - glsdc_traj(:, 5), 'DisplayName','GLSDC')
+plot(t, true_motion(:, 5) - monte_carlo_traj(:, 5), 'DisplayName','Monte Carlo')
+hold off
+title('$\dot{y}(t)$ Error', 'Interpreter','latex')
+ylabel('$\Delta \dot{y}$ (km)', 'Interpreter','latex')
+legend()
+grid()
+
+nexttile;
+hold on
+plot(t, true_motion(:, 6) - glsdc_traj(:, 6), 'DisplayName','GLSDC')
+plot(t, true_motion(:, 6) - monte_carlo_traj(:, 6), 'DisplayName','Monte Carlo')
+hold off
+title('$\dot{z}(t)$ Error', 'Interpreter','latex')
+xlabel('t (s)')
+ylabel('$\Delta \dot{z}$ (km)', 'Interpreter','latex')
+legend()
+grid()
+sgtitle('Trajectory Estimate Velocity Error')
+exportgraphics(gcf, 'Images/traj_estimate_vel_err.png', 'Resolution',300)
 
 %% Plotting Covariance Ellipses and covariance ellipsoid
 
 % Number of points to generate on ellipse
 npoints = 60;
 
-% Plotting Covariances along the y-z
-
+% Plotting Covariances for y-z
 figure
-hold on
+
+% Index tells to use x, y, or z [1, 2, or 3]
 idx = [2,3];
 
+hold on
+% Scale is the number of standard deviations
 for scale = 1:3
     glsdc_ellipse = cov_ellipse(glsdc_estimate(idx), P_glsdc(idx, idx), scale, npoints);
     plot(glsdc_ellipse(1, :), glsdc_ellipse(2, :), 'b')
@@ -190,19 +333,25 @@ for scale = 1:3
     monte_carlo_ellipse = cov_ellipse(monte_carlo_estimate(idx), P_mc(idx, idx), scale, npoints);
     plot(monte_carlo_ellipse(1, :), monte_carlo_ellipse(2, :), 'r')
 end
+
 scatter(glsdc_estimate(idx(1)), glsdc_estimate(idx(2)), 'b', 'filled', 'DisplayName','GLSDC Estimate')
 scatter(monte_carlo_estimate(idx(1)), monte_carlo_estimate(idx(2)), [], 'r', 'filled')
 scatter(y0(idx(1)), y0(idx(2)), 'k', '+', 'DisplayName', 'True Initial State')
+hold off
 title('Initial Position Estimate in y-z Plane')
 xlabel('y (km)')
 ylabel('z (km)')
 legend('GLSDC', '', '', 'Monte Carlo', '', '', '', '', 'True Initial State')
-
 exportgraphics(gca, 'Images/cov_yz.png', 'Resolution',300);
 
+% Plotting covariance for x-z
 figure
-hold on
+
+% Index tells to use x, y, or z [1, 2, or 3]
 idx = [1,3];
+
+hold on
+% Scale is the number of standard deviations
 for scale = 1:3
     glsdc_ellipse = cov_ellipse(glsdc_estimate(idx), P_glsdc(idx, idx), scale, npoints);
     plot(glsdc_ellipse(1, :), glsdc_ellipse(2, :), 'b')
@@ -213,16 +362,21 @@ end
 scatter(glsdc_estimate(idx(1)), glsdc_estimate(idx(2)), 'b', 'filled', 'DisplayName','GLSDC Estimate')
 scatter(monte_carlo_estimate(idx(1)), monte_carlo_estimate(idx(2)), [], 'r', 'filled')
 scatter(y0(idx(1)), y0(idx(2)), 'k', '+', 'DisplayName', 'True Initial State')
+
 title('Initial Position Estimate in x-z Plane')
 xlabel('x (km)')
 ylabel('z (km)')
 legend('GLSDC', '', '', 'Monte Carlo', '', '', '', '', 'True Initial State')
-
 exportgraphics(gca, 'Images/cov_xz.png', 'Resolution',300);
 
+% Plotting covariance for x-y
 figure
-hold on
+
+% Index tells to use x, y, or z [1, 2, or 3]
 idx = [1,2];
+
+hold on
+% Scale is the number of standard deviations
 for scale = 1:3
     glsdc_ellipse = cov_ellipse(glsdc_estimate(idx), P_glsdc(idx, idx), scale, npoints);
     plot(glsdc_ellipse(1, :), glsdc_ellipse(2, :), 'b')
@@ -233,13 +387,12 @@ end
 scatter(glsdc_estimate(idx(1)), glsdc_estimate(idx(2)), 'b', 'filled', 'DisplayName','GLSDC Estimate')
 scatter(monte_carlo_estimate(idx(1)), monte_carlo_estimate(idx(2)), [], 'r', 'filled')
 scatter(y0(idx(1)), y0(idx(2)), 'k', '+', 'DisplayName', 'True Initial State')
+
 title('Initial Position Estimate in x-y Plane')
 xlabel('x (km)')
 ylabel('y (km)')
 legend('GLSDC', '', '', 'Monte Carlo', '', '', '', '', 'True Initial State')
-
 exportgraphics(gca, 'Images/cov_xy.png', 'Resolution',300);
-
 
 %% Function Definitions
 
